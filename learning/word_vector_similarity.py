88d600199df1239aceb097fbe1068588b2a51a9f
O A <102542755+oboniA@users.noreply.github.com>
2023-03-29 16:59:59 +0100

    Add files via upload

---
+++ learning/word_vector_similarity.py
@@ -0,0 +1,27 @@
+"""
+        REMEMBER TO INSTALL spaCy and nlp LIBRARIES ON TERMINAL
+
+        python -m pip install spacy
+        python -m spacy download en_core_web_X  (X - sm/md/lg)
+
+        https://realpython.com/natural-language-processing-spacy-python/
+"""
+
+import spacy
+
+nlp = spacy.load("en_core_web_md")
+
+
+def show_similarity(base_word, textfile):
+
+    base_token = nlp(base_word)
+    doc = nlp(textfile)
+
+    for token in doc:
+        print(f"{token.text} <-> {base_token.text}:", token.similarity(base_token))
+
+
+token_word = "bread"
+texts = "I like eating egg mayo sandwich with crisps for lunch. However, I decided to go for hamburger today."
+
+show_similarity(token_word, texts)
